28fc407448c4a39d4b368855f77172070f41f80a
Ivan Uresti <ivan.uresti@gmail.com>
2020-01-31 00:45:32 -0600

    Add information to readme

--- src/uaslp/ingenieria/objetos/Main.java
+++ src/uaslp/ingenieria/objetos/Main.java
@@ -41,38 +41,3 @@ public class Main {
 
     }
 }
-/*
-Salida esperada:
-
-Materia: TECNOLOGÍA ORIENTADA A OBJETOS
-Clave: 223401
-Creditos: 8
-Profesor: Ivan Israel Uresti Adame
-Horario: 08-09
-----------------------------------
-Materia: TECNOLOGÍA ORIENTADA A OBJETOS
-Clave: 223402
-Creditos: 8
-Profesor: Francisco Javier Torres Reyes
-Horario: 09-10
-----------------------------------
-Materia: TECNOLOGÍA ORIENTADA A OBJETOS
-Clave: 223403
-Creditos: 8
-Profesor: Alberto Salvador Nuñez Varela
-Horario: 14-15
-----------------------------------
-Materia: ALGORITMOS Y COMPLEJIDAD
-Clave: 223501
-Creditos: 8
-Profesor: Miguel Angel De la Cruz
-Horario: 10-11
-----------------------------------
-Materia: Grafos
-Clave: 213301
-Creditos: 10
-Profesor: Miguel Angel Botello Aragón
-Horario: 11-12
-----------------------------------
-
- */
